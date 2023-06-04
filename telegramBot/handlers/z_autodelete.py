b09a362a40d1dd80fda4a9c3c7a044d37e1cda24
rslow <redmaxx4@gmail.com>
2023-06-05 00:15:44 +0900

    fix autodelete

--- telegramBot/handlers/z_autodelete.py
+++ telegramBot/handlers/z_autodelete.py
@@ -1,8 +1,8 @@
 from aiogram import types
 
 from bot import dispatcher
+from aiogram.types import ContentTypes
 
-
-@dispatcher.message_handler(state="*")
+@dispatcher.message_handler(state="*", content_types=ContentTypes.ANY)
 async def delete(message: types.Message):
     await message.delete()
